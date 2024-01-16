724f0cc100ebc0c3af1a3f21900401abd67ef6f4
RodrigoE92 <57400387+RodrigoE92@users.noreply.github.com>
2024-01-16 16:24:15 -0300

    Add files via upload
    primer commit

---
+++ Terraform testing/learn-terraform-azure/main.tf
@@ -0,0 +1,45 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  cloud {
+    organization = "rodrigoelvira"
+    workspaces {
+      name = "learn-terraform-azure"
+    }
+  }  
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  features {}
+}
+variable "subscription_id" {
+  default = "80e2a167-2569-48ed-b08b-09989abd758c"
+  type    = string
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "westus2"
+
+  tags = {
+    Environment = "Terraform Getting Started"
+    Team        = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
